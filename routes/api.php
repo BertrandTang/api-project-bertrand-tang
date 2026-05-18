85f7666be7809dd947b4ba7c782ffe60737faa6c
Bertrand <bertrand.tang.dev@gmail.com>
2026-05-18 17:21:05 +0200

    get book methods are now public

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ Route::get('/ping', function () {
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
@@ -21,9 +24,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [UserController::class, 'logout']);
 
-    Route::get('/books', [BookController::class, 'index']);
     Route::post('/books', [BookController::class, 'store']);
-    Route::get('/books/{book}', [BookController::class, 'show']);
     Route::put('/books/{book}', [BookController::class, 'update']);
     Route::patch('/books/{book}', [BookController::class, 'update']);
     Route::delete('/books/{book}', [BookController::class, 'destroy']);
