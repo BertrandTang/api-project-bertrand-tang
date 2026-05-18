4e193f8676b6fdd7186274654033310654f749eb
Bertrand <bertrand.tang.dev@gmail.com>
2026-05-18 11:41:51 +0200

    lint

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/ping', function () {
-return response()->json([
-'message' => 'pong',
-]);
+    return response()->json([
+        'message' => 'pong',
+    ]);
 });
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
